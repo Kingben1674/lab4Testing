b809313a23685702e3f50e83d41f04846ff8eb2e
Kingben1674 <124644364+Kingben1674@users.noreply.github.com>
2023-04-27 17:37:11 -0400

    Made more changes to JS

---
+++ testingapi.js
@@ -0,0 +1,21 @@
+const settings = {
+    "async": true,
+    "crossDomain": true,
+    "url": "https://coronavirus-smartable.p.rapidapi.com/stats/v1/US/",
+    "method": "GET",
+    "headers": {
+      "x-rapidapi-key": "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx",
+      "x-rapidapi-host": "coronavirus-smartable.p.rapidapi.com"
+    }
+  };
+  
+  $.ajax(settings).done(function (response) {
+    const stats = response.stats;
+    const totalConfirmedCases = stats.totalConfirmedCases;
+    const totalDeaths = stats.totalDeaths;
+    const totalRecoveredCases = stats.totalRecoveredCases;
+    console.log(`Total confirmed cases: ${totalConfirmedCases}`);
+    console.log(`Total deaths: ${totalDeaths}`);
+    console.log(`Total recovered cases: ${totalRecoveredCases}`);
+  });
+  
