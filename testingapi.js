8c5d5dcc7718c31998d7602470a6c89baeeed867
Kingben1674 <124644364+Kingben1674@users.noreply.github.com>
2023-04-29 11:29:31 -0400

    Working on session

--- testingapi.js
+++ testingapi.js
@@ -1,7 +1,10 @@
 
 window.onload = pageLoad;
 function pageLoad() {
+  if(sessionStorage.getItem('isLoggedIn') == 'true'){
     
+  }  
+
 }
 
 function onSignIn(googleUser) {
@@ -10,4 +13,7 @@ function onSignIn(googleUser) {
   console.log('Name: ' + profile.getName());
   console.log('Image URL: ' + profile.getImageUrl());
   console.log('Email: ' + profile.getEmail()); // This is null if the 'email' scope is not present.
+
+  sessionStorage.setItem('isLoggedIn', true); // set a session storage variable to indicate that the user is logged in
+  window.location.href = 'Secondpage.html'; // redirect to a new page
 }
